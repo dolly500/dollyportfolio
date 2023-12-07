e972cc89ac2154bb5e63d92999aa367841f94127
Akamo Dolapo <dolapoakamo01@gmail.com>
2023-12-07 19:18:19 +0900

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,6 +3,8 @@ import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/img/contact-img.svg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import emailjs from 'emailjs-com';
+
 
 export const Contact = () => {
   const formInitialDetails = {
@@ -25,23 +27,29 @@ export const Contact = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(formDetails),
-    });
-    setButtonText("Send");
-    let result = await response.json();
-    setFormDetails(formInitialDetails);
-    if (result.code == 200) {
-      setStatus({ succes: true, message: 'Message sent successfully'});
-    } else {
-      setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
+  
+    try {
+      await emailjs.send(
+        'service_ris2vwj', // Replace with your service ID
+        'template_alo301e', // Replace with your template ID
+        formDetails,
+        'Qr40OaloluqTkfltN' // Replace with your user ID
+      );
+  
+      setButtonText('Send');
+      setFormDetails(formInitialDetails);
+      setStatus({ success: true, message: 'Message sent successfully' });
+
+      // Reload the page after a successful submission
+    window.location.reload();
+    } catch (error) {
+      setStatus({
+        success: false,
+        message: 'Something went wrong, please try again later.',
+      });
     }
   };
-
+  
   return (
     <section className="contact" id="connect">
       <Container>
@@ -58,22 +66,27 @@ export const Contact = () => {
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <h2>Get In Touch</h2>
-                <form method="POST" action="https://forms.un-static.com/forms/2bd9617b61fbe85abce46461e3ac7a8ad60016be">
+                <form method="POST" action="" onSubmit={handleSubmit}>
                   <Row>
                     <Col size={12} sm={6} className="px-1">
-                      <input type="text" placeholder="First Name"  />
+                      <input type="text" placeholder="First Name" value={formDetails.firstName}
+                    onChange={(e) => onFormUpdate('firstName', e.target.value)} />
                     </Col>
                     <Col size={12} sm={6} className="px-1">
-                      <input type="text" placeholder="Last Name" />
+                      <input type="text" placeholder="Last Name" value={formDetails.lastName}
+                    onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
                     </Col>
                     <Col size={12} sm={6} className="px-1">
-                      <input type="email" placeholder="Email Address"  />
+                      <input type="email" placeholder="Email Address" value={formDetails.email}
+                    onChange={(e) => onFormUpdate('email', e.target.value)} />
                     </Col>
                     <Col size={12} sm={6} className="px-1">
-                      <input type="tel"  placeholder="Phone No."/>
+                      <input type="tel"  placeholder="Phone No." value={formDetails.phone}
+                    onChange={(e) => onFormUpdate('phone', e.target.value)}/>
                     </Col>
                     <Col size={12} className="px-1">
-                      <textarea rows="6" placeholder="Message"></textarea>
+                      <textarea rows="6" placeholder="Message" value={formDetails.message}
+                    onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
                       <button type="submit"><span>{buttonText}</span></button>
                     </Col>
                     {
